b3bd4d4465471144715018e4886a047b2e410464
Robin Andeer <robin.andeer@gmail.com>
2014-12-21 10:47:52 +0100

    refactor builder pipeline

--- chanjo/builder/core.py
+++ chanjo/builder/core.py
@@ -41,18 +41,34 @@ def init_db(chanjo_db, bed_stream, overwrite=False):
   # set up new tables
   chanjo_db.set_up()
 
-  superblocks = pipe(
-    bed_stream,
-    map(text_type.rstrip),
-    map(split(sep='\t')),
-    map(lambda row: bed_to_interval(*row)),
-    map(build_interval(chanjo_db)),
-    concat,
-    aggregate,
-    map(build_block(chanjo_db)),
-    aggregate,
-    map(build_superblock(chanjo_db))
-  )
+  # strip invisble characters
+  cleaned_up = (line.rstrip() for line in bed_stream)
+
+  # split lines
+  splitted = (text_type.split(line) for line in cleaned_up)
+
+  # convert to objects
+  objectified = (bed_to_interval(*row) for row in splitted)
+
+  # build interval objects and load into database
+  nested_intervals = (build_interval(chanjo_db, interval)
+                      for interval in objectified)
+
+  # flatten the nested list of lists of intervals
+  flattened = concat(nested_intervals)
+
+  # aggregate intervals based on common block IDs
+  block_groups = aggregate(flattened)
+
+  # build interval objects and load into database
+  blocks = (build_block(chanjo_db, group) for group in block_groups)
+
+  # aggregate blocks based on common superblock IDs
+  superblock_groups = aggregate(blocks)
+
+  # build interval objects and load into database
+  superblocks = (build_superblock(chanjo_db, group)
+                 for group in superblock_groups)
 
   # reduce the superblocks and commit every contig
   reduce(commit_per_contig(chanjo_db), superblocks, 'chr0')
